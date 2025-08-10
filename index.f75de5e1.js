5bb74037b6ab49d79bf4a2adf4aef0eea0e25edc
Nazariy <nazariypanochko@gmail.com>
2025-08-10 19:28:38 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+const getHeaderLi = document.querySelectorAll("li");
+getHeaderLi.forEach((li)=>{
+    const span = document.createElement("span");
+    span.textContent = li.firstChild.textContent.trim();
+    li.firstChild.replaceWith(span);
+    span.addEventListener("click", ()=>{
+        const ul = li.querySelector("ul");
+        if (ul) ul.hidden = !ul.hidden;
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
